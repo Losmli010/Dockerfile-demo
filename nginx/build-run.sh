85398982d3d47ef57ea710249a87c96efadf2e10
cdj <1060018592@qq.com>
2021-03-29 20:51:23 +0800

    更新配置

--- nginx/build-run.sh
+++ nginx/build-run.sh
@@ -4,6 +4,6 @@ docker build -t nginx-html:v1.0 .
 docker container run -d --name docker-nginx-v1 -v nginx-volume:/var/log/nginx -p 8080:80 nginx-html:v1.0
 
 # 更新网页信息，升级镜像
-docker build -t nginx-html:v2.0 .
+docker build -t nginx-html:v1.0 .
 # 运行容器
-docker container run -d --name docker-nginx-v2 -v nginx-volume:/var/log/nginx -p 8080:80 nginx-html:v2.0
+docker container run -d --name docker-nginx-v2 -v nginx-volume:/var/log/nginx -p 8080:80 nginx-html:v1.0
